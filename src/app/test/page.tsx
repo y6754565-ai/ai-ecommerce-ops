2b8cc613177f60ce7ff73afb8386a88ecc6b17d3
杨洋 <yangyang@yangyangdeMacBook-Air-2.local>
2026-06-04 15:10:19 +0800

    debug: test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1 style={{ fontSize: 24, fontWeight: 700 }}>测试页面</h1>
+      <p style={{ marginTop: 12, color: "#666" }}>看到这段文字说明部署正常。</p>
+    </div>
+  );
+}
